670442bd280c2d18cab1aa1f504922c93f0f95b9
Antoine Caron-Guay <antoine.caron-guay@umontreal.ca>
2025-06-02 14:48:46 -0400

    Copied from https://github.com/traitlab/lefolab-dji-waypoints/commit/2e5f0b1c5f262f6badecd8148399064c3bfbf71d

---
+++ src/model/config.py
@@ -0,0 +1,30 @@
+import re
+from pathlib import Path
+
+from pydantic import BaseModel, Field, FilePath, field_validator
+from typing_extensions import Annotated
+
+
+class Config(BaseModel):
+    approach: int
+    buffer: int
+
+    base_path: Path
+    base_name: str
+
+    points_csv_file_path: FilePath
+
+    touch_sky: bool
+    touch_sky_interval: int
+    touch_sky_altitude: float
+
+    kml_model_file_path: FilePath = './scripts/wpml/model/onewpt-Remote.kmz.org/wpmz/template.kml'
+    output_kml_file_path: Path = 'wpmz/template.kml'
+    wpml_model_file_path: FilePath = './scripts/wpml/model/onewpt-Remote.kmz.org/wpmz/waylines.wpml'
+    output_wpml_file_path: Path = 'wpmz/waylines.wpml'
+
+
+    debug_mode: bool = False
+
+    class Config:
+        arbitrary_types_allowed = True
